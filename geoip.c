c7f2c0d0744e5479b3e1f775c0d579e4107dba8e
root <tx@mediaone.bg>
2011-12-27 15:55:11 +0200

    add cache

--- geoip.c
+++ geoip.c
@@ -2,7 +2,7 @@
 
 char *lookup_real(char *ip) {
 	GeoIP * gi;
-	gi = GeoIP_new(GEOIP_STANDARD);
+	gi = GeoIP_new(GEOIP_MEMORY_CACHE);
 	const char * result;
 	result = GeoIP_country_code_by_name(gi, ip);
 	GeoIP_delete(gi);
