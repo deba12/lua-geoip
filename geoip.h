eb9c26ad6a9810b8da35c2a39cb724d86857b80d
root <tx@mediaone.bg>
2011-12-28 11:28:31 +0200

    cleanup

--- geoip.h
+++ geoip.h
@@ -1,13 +1,7 @@
-#ifndef CRC_H
-#define CRC_H
+#ifndef LGEOIP_H
+#define LGEOIP_H
 #include <GeoIP.h>
 
-/**
- * Calculates the CRC32 of a given data
- * @param begin The input data
- * @param count The length of the data
- * @return The calculated CRC32
- */
 char *lookup_real(char *ip, const GeoIP *gi);
 
 #endif
